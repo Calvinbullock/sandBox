aae42ec5c4a2f82ca91769a6476e6dad81b695b0
Calvinbullock <calvinnbullock@gmail.com>
2025-02-17 16:09:37 -0700

    prep for division

--- intAsArray.py
+++ intAsArray.py
@@ -40,6 +40,10 @@ def array_to_int(ansArray):
     return int(ans)
 
 
+# ----------------------------------------- #
+#                 ADDING                    #
+# ----------------------------------------- #
+
 def add(a,b):
     """
     Wrapper for add_array
@@ -89,6 +93,10 @@ def add_array(a, b):
 
     return ans
 
+# ----------------------------------------- #
+#                 MULTIPLY                  #
+# ----------------------------------------- #
+
 
 def multiply(a, b):
     """
@@ -137,6 +145,9 @@ def multiply_array(a, b):
 
     return product
 
+# ----------------------------------------- #
+#                 SUBTRACT                  #
+# ----------------------------------------- #
 
 def subtract(a, b):
     """
@@ -166,6 +177,59 @@ def subtract_array(a, b):
     Return
         int[]: An array answer
     """
+    while len(a) > len(b):
+        b.append(0)
+
+    while len(a) < len(b):
+        a.append(0)
+
+    ans = []
+
+    for i in range(0, len(a)):
+        if a[i] < b[i] and i+1 < len(a):
+            a[i+1] -= 1 # need to protect
+            a[i] += 10
+            diff = (a[i] - b[i])
+
+        else:
+            diff = (a[i] - b[i])
+
+        ans.append(diff)
+
+    return ans
+
+# ----------------------------------------- #
+#                 DIVISION                  #
+# ----------------------------------------- #
+
+def division(a, b):
+    """
+    Wrapper for division_array
+
+    Args
+        A: int
+        B: int
+
+    Return
+        Int: number
+    """
+    a = split_digits_reversed(a)
+    b = split_digits_reversed(b)
+    return array_to_int(division_array(a, b))
+
+
+def division_array(a, b):
+    """
+    takes two arrays containing a number each (can be thousands of digits)
+        and divides them from each other.
+
+    Args
+        A: int[]
+        B: int[]
+
+    Return
+        int[]: An array answer
+    """
     # TODO: this is all addition logic so far...
 
     while len(a) > len(b):
@@ -187,13 +251,22 @@ def subtract_array(a, b):
 
         ans.append(diff)
 
-    print(ans)
     return ans
 
 
 # ----------------------------------------- #
 #           TESTING Functions               #
 # ----------------------------------------- #
+
+def divistion_test(a, b, true_ans):
+    """
+    """
+    ans = division(a, b)
+
+    print()
+    print(a, "/", b, "=", true_ans)
+    print("ans =", ans, end=" --")
+    print(" ✔️ " if true_ans == ans else " ❗")
 
 def subtraction_test(a, b, true_ans):
     """
@@ -213,7 +286,7 @@ def multiply_test(a, b, true_ans):
 
     print()
     print(a, "*", b, "=")
-    print(true_ans, "==", ans)
+    print(true_ans, "==", ans, end="")
     print(" ✔️ " if true_ans == ans else " ❗")
 
 
@@ -224,7 +297,7 @@ def add_test(a, b, true_ans):
 
     print()
     print(a, "+", b, "=")
-    print(true_ans, "==", ans)
+    print(true_ans, "==", ans, end="")
     print(" ✔️ " if true_ans == ans else " ❗")
 
 
@@ -232,26 +305,34 @@ def add_test(a, b, true_ans):
 #                RUN TESTS                  #
 # ----------------------------------------- #
 
-# SUBTRACTION - TEST
-subtraction_test(3, 1, 2)               # simple
-subtraction_test(18, 4, 14)             # double - single digits
-subtraction_test(22, 4, 18)             # double - single digits / borrow * 1
-subtraction_test(133, 44, 89)           # triple - double digits / borrow * 2
-subtraction_test(133, 333, -200)        # neg ans / borrow * 2
-subtraction_test(33, 133, -100)         # neg ans / double - triple
-subtraction_test(33, 10033, -10000)     # neg ans / penta - double digits
+# Division - TEST
+divistion_test(4, 2, 2)                 # Simple
+divistion_test(80, 2, 40)               # double
+divistion_test(80, 80, 1)               # double doubles
+divistion_test(8000, 20, 400)           # big numbers
 
-# MULTIPLICATION - TEST
+# SUBTRACTION - TEST - PASSING
+# subtraction_test(3, 1, 2)               # simple
+# subtraction_test(18, 4, 14)             # double - single digits
+# subtraction_test(22, 4, 18)             # double - single digits / borrow * 1
+# subtraction_test(133, 44, 89)           # triple - double digits / borrow * 2
+# subtraction_test(133, 333, -200)        # neg ans / borrow * 2
+# subtraction_test(33, 133, -100)         # neg ans / double - triple
+# subtraction_test(33, 10033, -10000)     # neg ans / penta - double digits
+# print("-- Subtraction --")
+
+# MULTIPLICATION - TEST - PASSING
 # multiply_test(1, 2, 2)
 # multiply_test(22, 22, 484)
-# multiply_test(20000000000000000000000000000000000, 20000000000000000000000000000000000, 400000000000000000000000000000000000000000000000000000000000000000000)
+# print("-- Mutiplication --")
 
-# ADDING - TEST
+# ADDING - TEST - PASSING
 # add_test(1, 3, 4)
 # add_test(99, 11, 110)
 # add_test(12, 12, 24)
 # add_test(19, 11, 30)
 # add_test(1, 12, 13)
 # add_test(12, 2, 14)
+# print("-- Addition --")
 
 
